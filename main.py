a08b260a0ed156cf4e53e0ed74fc4b45571611cc
David Carboni <david@carboni.io>
2016-08-06 15:21:04 +0100

    More or less working, but no UI yet, so expect a black screen. You'll need to press A or B.

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
 from http_client import *
+from buttons import *
+from dialogs import *
+
+
+def wiggle():
+    request("Let's give it some wiggle!", "Hopefully you saw the lights wiggle!!", "/lights")
+
+
+def disco():
+    request("Let's get disco, honey!", "Hopefully you saw some disco action!!", "/disco")
+
+
+def request(before, after, uri):
+    notice(before)
+    try:
+        response = get("http://carboni.io/" + uri).raise_for_status().content
+        print("EMF number one says: " + repr(response))
+        notice("Got a reply from emf number one: " + repr(response))
+        notice(after)
+        print("finish!")
+    except Exception as ex:
+        print("Error: " + repr(ex))
+        notice("Aw shoot, we got an error: " + repr(ex))
+
+
+while True:
+    if is_triggered("BTN_A"):
+        wiggle()
+    elif is_triggered("BTN_B"):
+        disco()
+
+
