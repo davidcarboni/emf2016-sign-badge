f03ea5c35372d4317c6f5f38ac15d2cfad22480d
David Carboni <david@carboni.io>
2016-08-06 15:21:45 +0100

    Rather better transfer script.

--- transfer.sh
+++ transfer.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-cp main.py /media/david/PYBFLASH/apps/lights/
+
+if [ -d /media/$USER/PYBFLASH/apps ]
+then
+  mkdir -p /media/$USER/PYBFLASH/apps/lights
+  cp main.py /media/$USER/PYBFLASH/apps/lights/ && echo Code transferred.
+else
+  echo "Can't find /media/$USER/PYBFLASH/apps/ - is the device plugged in?"
+fi
